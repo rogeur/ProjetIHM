bbff609d40260234cf034676b0b2d750761fd2f1
rogeur <rsolivier@gmail.com>
2018-10-11 12:29:28 +0200

    modificatio du css pour les petite carte

--- src/app/film/film.component.ts
+++ src/app/film/film.component.ts
@@ -34,4 +34,42 @@ export class FilmComponent implements OnInit {
     return `https://image.tmdb.org/t/p/w500${path}`;
   }
 
+  getTitle(): String {
+    return this._movie.title;
+  }
+  getSummary(): String {
+    return this._movie.overview;
+  }
+
 }
+
+//
+// import { Component, OnInit } from '@angular/core';
+//
+// @Component({
+//   selector: 'app-small-card',
+//   templateUrl: './small-card.component.html',
+//   styleUrls: ['./small-card.component.css']
+// })
+// export class SmallCardComponent implements OnInit {
+//   date: Date;
+//   title: string;
+//   path: string;
+//   description: string;
+//   constructor() {
+//     this.title = 'Kill Bill';
+//     this.path = 'assets/killBill.jpg';
+//     this.description = 'Au cours d\'une cérémonie de mariage en plein désert, ' +
+//       'un commando fait irruption dans la chapelle et tire sur les convives.' +
+//       ' Laissée pour morte, la Mariée enceinte retrouve ses esprits après un coma de quatre ans.';
+//   }
+//
+//   ngOnInit() {
+//     this.pretty();
+//   }
+//   pretty () {
+//     this.description = this.description.slice(0, 75) + ' ...';
+//   }
+//
+// }
+
