4c6b75073b4d078ff97645c94cf1d91b5afd3058
leobello <leobello.wd@gmail.com>
2018-10-17 22:17:03 +0200

    recherche de film OK

--- src/app/header-menu/header-menu.component.ts
+++ src/app/header-menu/header-menu.component.ts
@@ -1,10 +1,9 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {TmdbService} from '../tmdb.service';
-import {MovieResponse} from '../tmdb-data/Movie';
 import {NgForm} from '@angular/forms';
-import {SearchPeopleQuery} from '../tmdb-data/SearchPeople';
 import {SearchMovieResponse} from '../tmdb-data/searchMovie';
+
 
 @Component({
   selector: 'app-header-menu',
@@ -14,39 +13,24 @@ import {SearchMovieResponse} from '../tmdb-data/searchMovie';
 
 export class HeaderMenuComponent implements OnInit {
 
-  private myQuery: number;
-  private encode: any;
+  private myQuery = ({
+    query: ''
+  });
   private movies: SearchMovieResponse;
-  private movie: MovieResponse;
   constructor(private router: Router, private tmdb: TmdbService) { }
   ngOnInit() {
   }
-
   handleClick() {
     this.router.navigateByUrl('playlistMenu');
   }
-
   onSubmit(form: NgForm) {
-    this.myQuery = form.value.searchReq;
-    /* verification de l'encodage */
-    // this.encode = encodeURI(this.myQuery);
-    // console.log(this.encode);
-    // console.log(decodeURI(this.myQuery));
-    console.log(this.myQuery);
-    this.searchById();
+    this.myQuery.query = form.value.searchReq;
+    this.searchByName();
   }
-
   searchByName() {
     this.tmdb.init('76a1a345942fd69cde4370065fed299e')
-      .searchMovie(this.encode)
-      .then( (m: SearchMovieResponse) => this.movies = m )
-      .catch(err => console.error('Error getting movie:', err) );
-  }
-
-  searchById() {
-    this.tmdb.init('76a1a345942fd69cde4370065fed299e')
-      .getMovie(this.myQuery)
-      .then( (m: MovieResponse) => console.log('Movie 13:', this.movie = m) )
+      .searchMovie(this.myQuery)
+      .then( (m: SearchMovieResponse) => console.log('result: ', this.movies = m ) )
       .catch(err => console.error('Error getting movie:', err) );
   }
 }
