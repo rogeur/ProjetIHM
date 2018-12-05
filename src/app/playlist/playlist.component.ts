7b5f637a4e34f3961148c6d4a8c1f3ed6b231926
benio73 <benjibendu73@hotmail.fr>
2018-12-05 08:43:29 +0100

    supprimer une playlist directement

--- src/app/playlist/playlist.component.ts
+++ src/app/playlist/playlist.component.ts
@@ -1,4 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {PlaylistService} from '../playlist.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-playlist',
@@ -11,8 +13,13 @@ export class PlaylistComponent implements OnInit {
   @Input() playlistName: string;
   @Input() index: number;
 
-  constructor() { }
+  constructor(private router: Router, private playlistService: PlaylistService) { }
 
   ngOnInit() {
+  }
+
+  handleClickRemove() {
+    this.playlistService.delPlaylistByName(this.playlistName);
+    this.router.navigate(['home']);
   }
 }
